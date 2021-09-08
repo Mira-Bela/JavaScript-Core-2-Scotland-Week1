6d8719051bc07bc37bdad0f032be756b38967838
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2021-09-08 16:01:00 +0100

    A - Objects

--- Class/A-Objects/exercise2.js
+++ Class/A-Objects/exercise2.js
@@ -1,17 +1,23 @@
-
 // Here is the example from the last exercise
 let dog1 = {
-    breed: "Dalmatian",
-    name: "Marshal",
-    isHungry: true,
-    happiness: 6
+  breed: "Dalmatian",
+  name: "Marshal",
+  isHungry: true,
+  happiness: 6,
 };
 
 // Define your own Object called dog2 with the same properties as the object above
 // The properties should have different values
 
-
+let dog2 = {
+  breed: "Golden Retriever",
+  name: "Max",
+  isHungry: true,
+  happiness: 8,
+};
 // Log the value of each property
 
-
-
+console.log(dog2.breed);
+console.log(dog2.name);
+console.log(dog2.isHungry);
+console.log(dog2.happiness);
